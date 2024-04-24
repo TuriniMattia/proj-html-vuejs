55ad02e69dee2bbceb2c363c5de6b01ac4b1c79e
TuriniMattia <turinimattia96@gmail.com>
2024-04-24 15:44:09 +0200

    [ADD]black-sections

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -43,11 +43,45 @@
                 </div>
                 <!-- div con all'interno una card piu grossa che occupa il 60% del container , con un paragrafo e un bottone -->
                 <!-- una card con all'interno 3 card piu piccole -->
-                <div class="d_flex">
-                    <div>
-                        ciao
+                <div class="d_flex mnsc_footer">
+                    <div class="rew_card ">
+                        <h5>FEATURED ARTICLE</h5>
+                        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, quibusdam corporis? Sed
+                            voluptatum dicta
+                            ad corporis expedita pariatur possimus soluta iure nobis cumque repudiandae, porro
+                            distinctio, quod fugit
+                            laudantium ipsam?
+                        </p>
+                        <button class="button black">READ MORE > </button>
                     </div>
-                    <div>
+                    <div class="tutorial_card ">
+                        <h2>Tutorials & Guides</h2>
+                        <hr class="red_bar">
+                        <div class="card_container_column ">
+                            <div class="card d_flex">
+                                <img src="/public/post_feat_img_23-147x118.jpg" alt="">
+                                <div class="card_text_container">
+                                    <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h5>
+                                    <p class="card_text">October, 11th,2015 &VerticalLine; Comments off</p>
+                                </div>
+
+                            </div>
+                            <div class="card d_flex">
+                                <img src="/public/post_feat_img_22-147x118.jpg" alt="">
+                                <div class="card_text_container">
+                                    <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h5>
+                                    <p class="card_text">October, 11th,2015 &VerticalLine; Comments off</p>
+                                </div>
+                            </div>
+                            <div class="card d_flex">
+                                <img src="/public/post_feat_img_20-147x118.jpg" alt="">
+                                <div class="card_text_container">
+                                    <h5>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </h5>
+                                    <p class="card_text">October, 11th,2015 &VerticalLine; Comments off</p>
+                                </div>
+                            </div>
+                        </div>
 
                     </div>
 
@@ -59,8 +93,9 @@
 
         </section>
         <!-- sezione con sfondo nero e un'ancora  -->
-        <section>
-            <!-- <a href=""></a> -->
+
+        <section class="black_section">
+            <h4><a href="">READ OUR BLOG &RightArrow;</a></h4>
         </section>
         <!-- forum section contenente una serie di card tutte uguali che occupano tutto il container e con icone all'interno -->
         <section>
@@ -168,7 +203,7 @@ export default {
 .red_bar {
     width: 80px;
     border-color: rgb(255, 141, 97);
-    margin-top: 50px;
+    margin-top: 30px;
 
 }
 
@@ -192,4 +227,56 @@ export default {
 }
 
 
-//MAIN_SECTION FOOTER //</style>
+//MAIN_SECTION FOOTER //
+.mnsc_footer {
+    margin-top: 80px;
+}
+
+.rew_card {
+    width: calc(8 * 100% / 12);
+    height: 500px;
+    background-image: url(/public/featured_article_2_bg.jpg);
+    background-size: 100%;
+    color: white;
+    padding: 80px 80px;
+    line-height: 250%;
+}
+
+.tutorial_card {
+    width: calc(4 * 100% / 12);
+    height: 500px;
+    padding-left: 60px;
+
+}
+
+.card_container_column {
+    display: flex;
+    flex-direction: column;
+
+}
+
+.card {
+    margin-top: 25px;
+}
+
+.card_text_container {
+    padding-left: 10px;
+    text-align: left;
+}
+
+.card_text {
+    font-size: 12px;
+    word-spacing: 8px;
+    padding-top: 5px;
+    color: rgb(112, 112, 112);
+}
+
+// BLACK SECTION //
+
+.black_section {
+    color: white;
+    background-color: black;
+    text-align: center;
+    padding: 30px;
+}
+</style>
